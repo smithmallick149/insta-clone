5aa5600e63e6174482aabc7f11c29ae5f43712a6
smith@ariveguru.com <smith@ariveguru.com>
2020-05-27 01:44:06 +0530

    private token auth and createpost getposts getpost:id and schemas corrected

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,23 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin')
-
+const Post = mongoose.model("Post")
 
 
 
 //create routes
+
+//get all posts
+router.get('/allpost', requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
+    .sort('-createdAt')
+    .then((posts) => {
+      res.json({ posts })
+    }).catch(err => {
+      console.log(err)
+    })
+
+})
+//creating post route
 router.post('/createpost', requireLogin, (req, res) => {
   const { title, body } = req.body
   if (!title || !body) {
-    res.status(422).json({ error: "please add all fields" })
+    return res.status(422).json({ error: "please add all the fields" })
   }
-  const post = new post({
+  req.user.password = undefined
+
+  const post = new Post({
     title,
-    body
+    body,
+    // photo: pic,
+    postedBy: req.user
   })
+  console.log(req.user)
+  post.save().then(result => {
+    res.json({ post: result })
+  })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+//get my post - with respect to id
+router.get('/mypost', requireLogin, (req, res) => {
+  console.log(req.user.name)
+  Post.find({ postedBy: req.user._id })
+    .populate("PostedBy", "_id name")
+    .then(mypost => {
+      res.json({ mypost })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
 module.exports = router
